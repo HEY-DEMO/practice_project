f3ae6a707805b6e674bdbd2dcf4bbfb804f0f407
unknown <kodurutharun18@gmail.com>
2023-12-12 14:19:04 +0530

    Added practice project for handling web elements and file uploading

---
+++ practice_project_file_upload_handle_webelements/src/main/java/project_file_upload_and_handle_webelements/App.java
@@ -0,0 +1,13 @@
+package project_file_upload_and_handle_webelements;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
